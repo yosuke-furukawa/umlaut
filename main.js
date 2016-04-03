d79e9c99d41859112cbe29503659bd3eade47b3e
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2016-04-03 18:16:41 +0900

    Remove debug console

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600, });
 
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-mainWindow.webContents.openDevTools();
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
